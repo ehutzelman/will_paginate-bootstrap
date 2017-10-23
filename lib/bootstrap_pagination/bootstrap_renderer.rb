b23f4243abcb8fbef921bf98a286014e9c36c8f5
Eric Hutzelman <ehutzelman@gmail.com>
2017-10-23 13:30:45 -0500

    Changes for bootstrap v4
    merged in from https://github.com/NetherLinks/will_paginate-bootstrap

--- lib/bootstrap_pagination/bootstrap_renderer.rb
+++ lib/bootstrap_pagination/bootstrap_renderer.rb
@@ -3,19 +3,15 @@ require "bootstrap_pagination/version"
 module BootstrapPagination
   # Contains functionality shared by all renderer classes.
   module BootstrapRenderer
-    ELLIPSIS = "&hellip;"
 
     def to_html
-      list_items = pagination.map do |item|
-        case item
-          when 0.class
-            page_number(item)
-          else
-            send(item)
-        end
+      html = pagination.map do |item|
+        item.is_a?(Fixnum) ? page_number(item) : send(item)
       end.join(@options[:link_separator])
 
-      tag("ul", list_items, class: ul_class)
+      html = html_container(html) if @options[:container]
+
+      tag("nav", tag("ul", html, class: ul_class))
     end
 
     def container_attributes
@@ -28,9 +24,9 @@ module BootstrapPagination
       link_options = @options[:link_options] || {}
 
       if page == current_page
-        tag("li", tag("span", page), class: "active")
+        tag("li", link(page, "#", class: "page-link"), class: "page-item active")
       else
-        tag("li", link(page, page, link_options.merge(rel: rel_value(page))))
+        tag("li", link(page, page, link_options.merge(rel: rel_value(page), class: "page-link")), class: "page-item")
       end
     end
 
@@ -38,24 +34,24 @@ module BootstrapPagination
       link_options = @options[:link_options] || {}
 
       if page
-        tag("li", link(text, page, link_options), class: classname)
+        tag("li", link(text, page, link_options.merge(class: "page-link")), class: "%s page-item" % classname)
       else
-        tag("li", tag("span", text), class: "%s disabled" % classname)
+        tag("li", link(text, "#", class: "page-link"), class: "%s page-item disabled" % classname)
       end
     end
 
     def gap
-      tag("li", tag("span", ELLIPSIS), class: "disabled")
+      tag("li", link("&hellip;", "#", class: "page-link"), class: "page-item disabled")
     end
 
     def previous_page
       num = @collection.current_page > 1 && @collection.current_page - 1
-      previous_or_next_page(num, @options[:previous_label], "prev")
+      previous_or_next_page(num, @options[:previous_label], "Previous")
     end
 
     def next_page
       num = @collection.current_page < @collection.total_pages && @collection.current_page + 1
-      previous_or_next_page(num, @options[:next_label], "next")
+      previous_or_next_page(num, @options[:next_label], "Next")
     end
 
     def ul_class
